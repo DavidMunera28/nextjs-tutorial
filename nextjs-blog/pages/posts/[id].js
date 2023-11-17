e59664107f9ffadf19cdfc0ce83ef5740cc1c7c5
DavidMunera28 <dm663@njit.edu>
2023-11-17 12:44:00 -0500

    more changes

---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,23 @@
+import { getAllPostIds, getPostData } from '../../lib/posts';
+import Layout from '../../components/layout';
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id);
+    return {
+      props: {
+        postData,
+      },
+    };
+  }
+
+  export default function Post({ postData }) {
+    return (
+      <Layout>
+        {postData.title}
+        <br />
+        {postData.id}
+        <br />
+        {postData.date}
+      </Layout>
+    );
+  }
