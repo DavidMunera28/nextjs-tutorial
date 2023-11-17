25046b62d59a4ceb0406ec9acec7c19d9e142343
DavidMunera28 <dm663@njit.edu>
2023-11-17 08:31:40 -0500

    updates

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -10,3 +10,19 @@ export default function FirstPost() {
     </>
   );
 }
+
+import Head from 'next/head';
+
+export default function FirstPost() {
+    return (
+      <>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2>
+          <Link href="/">← Back to home</Link>
+        </h2>
+      </>
+    );
+  }
